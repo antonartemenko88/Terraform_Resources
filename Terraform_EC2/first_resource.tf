128da70ab48680f538b4b380759aa770c8a0de11
antonartemenko88 <artemenkoanton1997@gmail.com>
2021-05-05 18:35:56 -0500

    added files

---
+++ Terraform_EC2/first_resource.tf
@@ -0,0 +1,10 @@
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0d5eff06f840b45e9"
+  instance_type = "t2.micro"
+}
